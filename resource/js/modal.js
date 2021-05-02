c7b669cbedebbaa1ed703ae0d697c87b568f12c5
LeeHyeWoun <silro812@naver.com>
2021-05-03 03:43:22 +0900

    Update

--- resource/js/modal.js
+++ resource/js/modal.js
@@ -2,8 +2,7 @@ function changeModalText(skill_name) {
 
     let title = document.getElementsByClassName("modal_title")[0];
     title.innerText=skill_name;
-    console.log(skill_name);
- }
+}
  
 window.onload=function(){
     function modal(id) {

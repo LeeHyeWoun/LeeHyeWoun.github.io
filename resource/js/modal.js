05b68cddc5e3265110ad0dcab571e881307f6163
LeeHyeWoun <silro812@naver.com>
2021-05-06 22:37:44 +0900

    upadate

--- resource/js/modal.js
+++ resource/js/modal.js
@@ -21,10 +21,9 @@ window.onload=function(){
     fetch(url)
         .then((res)=>res.json())
         .then((data)=>{arr = data;})
-        .then(function(){
-            skills.classList.toggle(".skill_act");
-        })
         .catch(error=>console.error(error));
+    
+    skills.classList.toggle(".skill_act");
 
     function setContent(kind, num) {
         var content = arr[kind][num];
